0fb2b3bccb69a3852e169dd9ca8ea9df81e39c17
Eduardo-BDMAlves <53615213+Eduardo-BDMAlves@users.noreply.github.com>
2022-03-24 19:03:24 -0300

    Created struct for hexadecene, remember it is the same as cetene. Now create correlations...

--- src/Materials.jl
+++ src/Materials.jl
@@ -7,7 +7,8 @@ export TestSolid,
     Fluid_sets,
     BrineNaCl,
     CaCO3,
-    Hematite
+    Hematite,
+    Hexadecene
 
 
 abstract type Material end
@@ -158,7 +159,36 @@ end
 
 
 
+struct Hexadecene <: Fluid
 
+
+    name::Symbol
+    ρ::Float64
+    cₚ::Float64
+    μ::Float64
+    k::Float64
+
+    MM::Float64
+
+    #viscosity ver no arquivo c16.pdf
+
+    function Hexadecene()
+        MM=224.43
+
+        new(
+            :Hexadecene,
+            781.1,#no Nist 777.91
+            357.2/MM*1.0E3,
+            3.0E-3,
+            0.1423,
+            MM
+        )
+
+    end
+
+    #https://wtt-pro.nist.gov/wtt-pro/index.html?cmp=1-hexadecene#1-hexadecene;06;0H;0A;4g;1g;7g;3g;5g;Hg;8g,298.15,413.15,10;8b;O6;OA;OH;Qg;TK;TC;T2;Tg;Re;zK;zC;z2;zg;Ye;aK;aC;a2;ag;ae;hf;mg;jg;ja;na;ng;ya;yf/c;0,0/a;782,144/8gR;805,301/8gT;72,426,508,382/
+
+end
 
 
 
